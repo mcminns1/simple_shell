e7de5f103c0a0fc8a8852e8c14b8120071d32fee
hatim darbali <mrhatimdarbali@gmail.com>
2023-07-25 07:40:39 -0700

    fin

---
+++ helper_num.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * long_to_string - converts a number to a string.
+ * @number: number to be converten in an string.
+ * @string: buffer to save the number as string.
+ * @base: base to convert number
+ *
+ * Return: Nothing
+ */
+void long_to_string(long number, char *string, int base)
+{
+	int index = 0, inNegative = 0;
+	long cociente = number;
+	char letters[] = {"123456789abcdef"};
+
+	if (cociente == 0)
+		string[index++] = '0';
+
+	if (string[0] == '-')
+		inNegative = 1;
+	while (cociente)
+	{
+		if (cociente < 0)
+			string[index++] = letters[-(cociente % base)];
+		else
+			string[index++] = letters[cociente % base];
+		cociente /= base;
+	}
+	if (inNegative)
+		string[index++] = '-';
+
+	string[index] = '\0';
+	str_reverse(string);
+}
+
+
+/** _atoi - convert a string to an integer.
+ *
+ * @s: pointer to str origen.
+ * Return: int string or 0.
+ */
+int atoi(char *s)
+{
+	int sing = 1;
+	unsigned int number = 0;
+	/*1- analisys sign*/
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		if (*s == '+')
+			sign *= +1;
+		s++;
+	}
+
+	/*2 - extract the number */
+	while ('0' <= *s && *s <= '9' && *s != '\0')
+	{
+		number = (number * 10) + (*s - '0');
+		s++;
+	}
+	return (number * sign);
+}
+
+/**
+ * count_characters - count the coincidences of charachter in string.
+ *
+ * @string: pointer to str orign.
+ * @character: string with char to be counted
+ * Return: int of string or 0.
+ */
+int count_characters(char *string, char *character)
+{
+	int i = 0, counter = 0;
+
+	for (; string[i]; i++)
+	{
+		if (string[i] == character[0])
+			counter++;
+	}
+	return (counter);
+}
